c81764118be6f2b158033624c304b4305780e38c
Max Fortna <thexammer@gmail.com>
2025-10-05 00:19:55 -0400

    knife move

--- Assets/Scripts/MazeGameManager.cs
+++ Assets/Scripts/MazeGameManager.cs
@@ -21,6 +21,12 @@ public class MazeGameManager : MonoBehaviour
     public GameObject End_Goal;
     public Image Maze;
 
+    // max's messing around values
+    public Transform knife_transform;
+    private const float kKnifeLength = 0.5f;
+    private const float kAngleOffset = 60.0f;
+    public Vector2 LastToMouse;
+
     private void Start()
     {
         Maze.alphaHitTestMinimumThreshold = .75f;
@@ -28,6 +34,22 @@ public class MazeGameManager : MonoBehaviour
     public void EnableBadTouch()
     {
         TrackBadTouch = true;
+    }
+
+    public void Update()
+    {
+        if (TrackBadTouch)
+        {
+            Vector2 cur_pos = new Vector2( knife_transform.position.x, knife_transform.position.y );
+            Vector2 mouse_pos = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+            Vector2 to_mouse =  cur_pos - mouse_pos;
+            float angle = Vector2.SignedAngle(to_mouse.normalized, Vector2.right) + kAngleOffset;
+            LastToMouse = to_mouse;
+
+			Vector2 new_pos = (to_mouse.normalized * kKnifeLength) + mouse_pos;
+            knife_transform.position = new Vector3( new_pos.x, new_pos.y, knife_transform.position.z );
+            knife_transform.rotation = Quaternion.Euler(0.0f, 0.0f, -angle);
+        }
     }
 
     public void BadTouchCollision()
@@ -42,7 +64,7 @@ public class MazeGameManager : MonoBehaviour
 
         Sequence mySequence = DOTween.Sequence();
 
-        Failure[CurrentBadTouches - 1].DOShakeAnchorPos(1.5f, CurrentBadTouches*2, 10, 30);
+        Failure[CurrentBadTouches - 1].DOShakeAnchorPos(1.5f, CurrentBadTouches * 2, 10, 30);
         mySequence.Append(Failure[CurrentBadTouches - 1].DOScale(2, time))
             .Append(Failure[CurrentBadTouches - 1].DOScale(1, time / 2)).AppendInterval(time).OnComplete(() =>
             {
